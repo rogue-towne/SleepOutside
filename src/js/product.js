6108cb80e5f93c5c205d3c6663c265dc51aefbdc
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2023-03-03 14:38:45 -0700

    add item to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = (() => {
+    const cartItem = localStorage.getItem("so-cart");
+    return cartItem === null ? [] : getLocalStorage("so-cart");
+  })();
+
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
