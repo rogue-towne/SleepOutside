d9459b67353b3522ab274008c97be896c345b42c
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2023-03-04 18:20:34 -0700

    dynamic Product List

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+const dataSource = new ProductData("tents");
+const listElement = document.querySelector(".product-list");
+const productList = new ProductList("Tests", dataSource, listElement);
+
+productList.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,6 +21,20 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
 export function getParam(param){
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
